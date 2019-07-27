361dbf607a62c625e1a4f404ace7e99c8bd2d6ed
shenry45 <shawn45henry@gmail.com>
2019-07-26 19:28:43 -0700

    Post comment by post ID

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -30,22 +30,41 @@ router.post('/', async (req, res) => {
 
 router.post('/:id/comments', async (req, res) => {
   try {
-    if (!req.body.id) {
+    const {id} = req.params;
+    const commentEdit = {
+      ...req.body,
+      post_id: id
+    }
+
+    const idStatus = await db.findById(id);
+
+    if (!idStatus) {
       res.status(404).json({
         success: false,
         message: "The post with the specified ID does not exist."
       })
+    } else if (!req.body.text) {
+      res.status(400).json({
+        success: false,
+        errorMessage: "Please provide text for the comment."
+      })
     } else {
-      const postInfo = await db.insertComment(req.body);
+      console.log('before 2nd await');
+      const postCommInfo = await db.insertComment(commentEdit);
+      console.log('after 2nd await');
 
       res.status(201).json({
         success: true,
-        postInfo
+        postCommInfo
       })
     }
   }
   catch(err) {
-
+    res.status(500).json({
+      success: false,
+      error: "There was an error while saving the comment to the database",
+      err: err.message
+    })
   }
 })
 
